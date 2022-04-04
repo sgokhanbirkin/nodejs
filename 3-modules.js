d47137957669ecf17afe6f60957e2588876b7dcb
gokhanbirkin <gkandth@gmail.com>
2022-04-04 16:37:43 +0300

    part 1

---
+++ 3-modules.js
@@ -0,0 +1,13 @@
+
+const names = require('./4-names');
+const sayHi = require('./5-utils');
+const data = require('./6-alternative-flavor');
+require('./7-mind-grenade');
+
+
+
+// console.log(data)
+// sayHi('Susan');
+// sayHi(names.john);
+// sayHi(names.peter);
+
---
+++ 4-names.js
@@ -0,0 +1,9 @@
+// Local
+const secret = 'SUPER SECRET';
+// Share
+const john = 'John';
+const peter = 'Peter';
+
+// console.log(module)
+module.exports = { john , peter };
+
